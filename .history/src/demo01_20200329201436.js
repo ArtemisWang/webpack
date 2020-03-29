b68525aaf2b5e85b3cc7e96af2b49738815dbce1
ArtemisWang <xiao2sheng@qq.com>
2020-03-29 20:59:23 +0800

    art

---
+++ .history/src/demo01_20200329201436.js
@@ -0,0 +1,12 @@
+const sy=Symbol("sy")
+let syObject = {};
+syObject[sy] = "kk";
+console.log(syObject);
+ 
+for (let i in syObject) {
+  console.log(i);
+}    // 无输出
+ 
+Object.keys(syObject);                     // []
+Object.getOwnPropertySymbols(syObject);    // [Symbol(key1)]
+Reflect.ownKeys(syObject);                 // [Symbol(key1)]
